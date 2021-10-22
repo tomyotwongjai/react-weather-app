9ed87b5ddd95b72b577b54097780d22ed24ed9cb
Tom Yotwongjai <munch2018@gmail.com>
2021-10-22 13:56:27 +0200

    simple weather app
    Weather app us weatherapp api. User type in city and fetch current weather

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import './index.css';
+import ReactDOM from 'react-dom';
+
+const api = {
+  key: 'e902985907738b357b6a7c7a2651a108',
+  base: 'https://api.openweathermap.org/data/2.5/',
+};
+
+function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = (evt) => {
+    if (evt.key === 'Enter') {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  };
+
+  function dateBuilder() {
+    let date = String(new window.Date());
+    date = date.slice(0, 15);
+    return `${date}`;
+  }
+  return (
+    <>
+      <div
+        className={
+          typeof weather.main != 'undefined'
+            ? weather.main.temp > 16
+              ? 'app warm'
+              : 'app'
+            : 'app'
+        }
+      >
+        <main>
+          <div className='search-box'>
+            <input
+              type='text'
+              className='search-bar'
+              placeholder='Seach...'
+              onChange={(e) => setQuery(e.target.value)}
+              value={query}
+              onKeyPress={search}
+            />
+          </div>
+          {typeof weather.main != 'undefined' ? (
+            <div>
+              <div className='location-box'>
+                <div className='location'>
+                  {weather.name}, {weather.sys.country}
+                </div>
+                <div className='date'>{dateBuilder(new Date())}</div>
+              </div>
+              <div className='weather-box'>
+                <div className='temp'>{Math.round(weather.main.temp)}°</div>
+                <div className='weather'>{weather.weather[0].main}</div>
+              </div>
+            </div>
+          ) : (
+            ''
+          )}
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-const api = {
-  key: 'e902985907738b357b6a7c7a2651a108',
-  base: 'https://api.openweathermap.org/data/2.5/',
-};
-
-function App() {
-  return (
-    <div className='app'>
-      <main>
-        <div className='search-box'>
-          <input type='text' className='search-bar' placeholder='Seach...' />
-        </div>
-      </main>
-    </div>
-  );
-}
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
